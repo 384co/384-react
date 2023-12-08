21c9a88c64b3cecc26953d6161335e766a12d7d3
matt384 <108557364+matt384@users.noreply.github.com>
2023-12-08 10:35:33 -0800

    save

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import { stores } from "./stores/index";
+import SnackabraStore from "stores/Snackabra.Store";
+import ChannelStore from "stores/Channel.Store";
 
-export {stores};
+export type Stores = {
+    snackabraStore: SnackabraStore;
+    channelStore: ChannelStore;
+};
+
+export { stores };
+
